8dbb7a796d7783f737255447decc91fc4d656b0b
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-23 21:25:23 -0300

    Value colors . all

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hola");
+    let colors = new Values(color).all(10);
+    console.log(colors);
   };
 
   return (
